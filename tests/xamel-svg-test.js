60ce77c879577148df7022bddd4cae18f8685e5a
morgan craft <mgan59@gmail.com>
2014-01-29 10:00:55 -0500

    have a rough scaffold setup to triage nodes

---
+++ tests/xamel-svg-test.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var xamel = require('xamel');
+var SvgFactory = require('../index')();
+var colors = require('colors');
+
+var svgSrc = fs.readFileSync('../svg-examples/simple.svg', {encoding:'utf-8'});
+/*
+xamel.parse(svgSrc, function(err, xml) {
+    
+    console.log(err);
+    console.log('--');
+    //console.log(xml.children[1]);
+    var store = xml.children[1];
+    for(var ctr=0; ctr < store.children.length; ctr++){
+        console.log(store.children[ctr]);
+    }
+});
+*/
+
+SvgFactory.parse(svgSrc, {factoryName:'bigBunny'}, function(err, src){
+    
+    if(err){
+        console.log('the err? ',err);
+    }
+    
+   console.log('---'.blue); 
+    console.log(src.grey);
+
+});
+
